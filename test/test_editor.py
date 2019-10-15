21ad09b04e455754d8492991aa1f7db2adf1f390
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2019-10-15 14:14:09 +0200

    test: move editor related tests to their own module

---
+++ test/test_editor.py
@@ -0,0 +1,106 @@
+from datetime import datetime as dt
+
+import pytest
+import pandas as pd
+
+from tshistory.testutil import (
+    assert_df,
+    utcdt
+)
+
+from tshistory_formula.registry import (
+    func,
+    FUNCS
+)
+from tshistory_formula.editor import fancypresenter
+
+
+def test_editor_table_callback(engine, tsh):
+    groundzero = pd.Series(
+        [0, 0, 0],
+        index=pd.date_range(utcdt(2019, 1, 1), periods=3, freq='D')
+    )
+    one = pd.Series(
+        [1, 1, 1],
+        index=pd.date_range(utcdt(2019, 1, 1), periods=3, freq='D')
+    )
+    tsh.update(engine, groundzero, 'groundzero-a', 'Babar')
+    tsh.update(engine, one, 'one-a', 'Celeste')
+
+    tsh.register_formula(
+        engine,
+        'editor-1',
+        '(add (* 3.1416 (series "groundzero-a" #:fill "bfill" #:prune 1)) (series "one-a"))',
+    )
+
+    presenter = fancypresenter(engine, tsh, 'editor-1', {})
+    info = [
+        {
+            k: v for k, v in info.items() if k != 'ts'
+        }
+        for info in presenter.buildinfo()
+    ]
+    assert info == [
+        {'coef': 'x 3.1416', 'keywords': 'fill:bfill, prune:1',
+         'name': 'groundzero-a', 'type': 'primary'},
+        {'coef': 'x 1', 'keywords': '-', 'name': 'one-a', 'type': 'primary'},
+        {'coef': 'x 1', 'name': 'editor-1', 'type': 'formula: add'}
+    ]
+
+    # trigger an empty series
+    presenter = fancypresenter(engine, tsh, 'editor-1',
+                               {'from_value_date': utcdt(2019, 1, 4)})
+    info = [
+        {
+            k: v for k, v in info.items() if k != 'ts'
+        }
+        for info in presenter.buildinfo()
+    ]
+    assert info == [
+        {'coef': 'x 3.1416', 'keywords': 'fill:bfill, prune:1',
+         'name': 'groundzero-a', 'type': 'primary'},
+        {'coef': 'x 1', 'keywords': '-', 'name': 'one-a', 'type': 'primary'},
+        {'coef': 'x 1', 'name': 'editor-1', 'type': 'formula: add'}
+    ]
+
+
+def test_editor_no_such_series(engine, tsh):
+    with pytest.raises(AssertionError):
+        presenter = fancypresenter(engine, tsh, 'no-such-series', {})
+
+
+def test_editor_new_operator(engine, tsh):
+    @func('genrandomseries')
+    def genrandomseries():
+        return pd.Series(
+            [1.0, 2.0, 3.0],
+            index=pd.date_range(dt(2019, 1, 1), periods=3, freq='D')
+    )
+
+    tsh.register_formula(
+        engine,
+        'random',
+        '(genrandomseries)',
+        False
+    )
+
+    ts = tsh.get(engine, 'random')
+    assert_df("""
+2019-01-01    1.0
+2019-01-02    2.0
+2019-01-03    3.0
+""", ts)
+
+    presenter = fancypresenter(engine, tsh, 'random', {})
+    info = [
+        {
+            k: v for k, v in info.items() if k != 'ts'
+        }
+        for info in presenter.buildinfo()
+    ]
+    assert info == [
+        {'coef': 'x 1', 'name': 'random', 'type': 'formula: genrandomseries'}
+    ]
+
+    # cleanup
+    FUNCS.pop('genrandomseries')
