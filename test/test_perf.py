f645f9f9e911ade68be8294e878baa9398ae3152
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2020-02-25 19:38:59 +0100

    test/perf: more series for priority test
    Now that we have beefed up tshistory.util.patch
    (which makes this bench go from 6.6 to 0.9s)
    we need a bit more air.
    
    Next comes another optimisation (patchmany).

--- test/test_perf.py
+++ test/test_perf.py
@@ -22,6 +22,18 @@ def test_priority(engine, tsh):
             start=utcdt(2020, 6, 1), freq='D', periods=365 * 10
         )
     )
+    s3 = pd.Series(
+        data,
+        index=pd.date_range(
+            start=utcdt(2020, 9, 1), freq='D', periods=365 * 10
+        )
+    )
+    s4 = pd.Series(
+        data,
+        index=pd.date_range(
+            start=utcdt(2020, 12, 1), freq='D', periods=365 * 10
+        )
+    )
 
     tsh.update(
         engine,
@@ -35,14 +47,26 @@ def test_priority(engine, tsh):
         'prio2',
         'Celeste'
     )
+    tsh.update(
+        engine,
+        s3,
+        'prio3',
+        'Arthur'
+    )
+    tsh.update(
+        engine,
+        s4,
+        'prio4',
+        'Zéphir'
+    )
 
     tsh.register_formula(
         engine,
         'patch',
-        '(priority (series "prio2") (series "prio1"))'
+        '(priority (series "prio4") (series "prio3") (series "prio2") (series "prio1"))'
     )
     t0 = time()
     with engine.begin():
-        for _ in range(100):
+        for _ in range(300):
             patched = tsh.get(engine, 'patch')
     print('100 patches', time() - t0)
