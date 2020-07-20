aa24cbe504b1f2ff0f865e48d1a0f75fd07ea2f9
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2020-07-20 11:27:42 +0200

    pkg: prepare 0.7.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ doc = Path(__file__).parent / 'README.md'
 
 
 setup(name='tshistory_formula',
-      version='0.7.0',
+      version='0.7.1',
       author='Pythonian',
       author_email='aurelien.campeas@pythonian.fr',
       url='https://hg.sr.ht/~pythonian/tshistory_formula',
