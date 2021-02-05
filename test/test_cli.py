b9433b98ed205bedc2ac44f7fcd74fa0ccb6c55d
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2021-02-05 16:41:58 +0100

    test/cli: update-metadata

--- test/test_cli.py
+++ test/test_cli.py
@@ -27,3 +27,46 @@ def test_ingest(engine, cli, tsh, datadir):
 
     assert tsh.formula(engine, 'ones-imported')
     assert tsh.formula(engine, 'twos-imported')
+
+
+def test_update_metadata(engine, cli, tsh):
+    x = pd.Series(
+        [1, 2, 3],
+        index=pd.date_range(utcdt(2020, 1, 1), periods=3, freq='D')
+    )
+    tsh.update(engine, x, 'tzaware', 'Babar')
+
+    tsh.register_formula(
+        engine,
+        'tzaware-form',
+        '(series "tzaware")',
+    )
+
+    tsh.register_formula(
+        engine,
+        'tznaive-form',
+        '(+ 1 (naive (series "tzaware-form") "Europe/Paris"))',
+    )
+
+    m1 = tsh.metadata(engine, 'tzaware-form')
+    assert m1['tzaware']
+    m2 = tsh.metadata(engine, 'tznaive-form')
+    assert not m2['tzaware']
+
+    # let's do shit to the metadata
+    tsh.update_metadata(engine, 'tzaware-form', tsh.default_meta(False))
+    tsh.update_metadata(engine, 'tznaive-form', tsh.default_meta(True))
+
+    # flipped :o
+    m1 = tsh.metadata(engine, 'tzaware-form')
+    assert not m1['tzaware']
+    m2 = tsh.metadata(engine, 'tznaive-form')
+    assert m2['tzaware']
+
+    cli('update-formula-metadata', str(engine.url))
+
+    # flipped back o:
+    m1 = tsh.metadata(engine, 'tzaware-form')
+    assert m1['tzaware']
+    m2 = tsh.metadata(engine, 'tznaive-form')
+    assert not m2['tzaware']
