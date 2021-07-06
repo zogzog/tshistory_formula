e20b6031f3d63459bb0063bd1d157e3e49578611
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2021-07-06 16:49:37 +0200

    schema: introduce a schema for group formulas
    Series groups are a new tshistory feature.
    We also want to give them formulas.

--- tshistory_formula/schema.sql
+++ tshistory_formula/schema.sql
@@ -6,3 +6,12 @@ create table "{ns}".formula (
 );
 
 create unique index "ix_{ns}_formula_name" on "{ns}".formula (name);
+
+create table "{ns}".group_formula (
+  id serial primary key,
+  -- name will have an index (unique), sufficient for the query needs
+  name text unique not null,
+  text text not null,
+  metadata jsonb
+);
+
