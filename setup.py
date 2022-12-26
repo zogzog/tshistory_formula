2cd38e06faba054d7102e3499a6ed6948389e21c
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2022-12-26 14:17:26 +0100

    pkg: prepare 0.13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ doc = Path(__file__).parent / 'README.md'
 
 
 setup(name='tshistory_formula',
-      version='0.12.1',
+      version='0.13.0',
       author='Pythonian',
       author_email='aurelien.campeas@pythonian.fr',
       url='https://hg.sr.ht/~pythonian/tshistory_formula',
